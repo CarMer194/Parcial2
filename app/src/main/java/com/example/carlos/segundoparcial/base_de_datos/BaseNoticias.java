c238f26dc8e78844a92f6791ebd4d4c476e0c025
CarMer194 <00060313@uca.edu.sv>
2018-06-08 01:31:38 -0600

    agregado la base de datos

---
+++ app/src/main/java/com/example/carlos/segundoparcial/base_de_datos/BaseNoticias.java
@@ -0,0 +1,30 @@
+package com.example.carlos.segundoparcial.base_de_datos;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.example.carlos.segundoparcial.Noticias;
+import com.example.carlos.segundoparcial.Usuario;
+
+@Database(entities = {Usuario.class, Noticias.class}, version = 1)
+public abstract class BaseNoticias extends RoomDatabase {
+    public abstract UsuarioDao userDao();
+    public abstract NoticiasDao noticiasDao();
+
+    private static BaseNoticias INSTANCE;
+
+    public static BaseNoticias getDatabase(final Context context){
+        if (INSTANCE == null){
+            synchronized (BaseNoticias.class){
+                if (INSTANCE==null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            BaseNoticias.class,"base_de_noticias").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+}
--- app/src/main/java/com/example/carlos/segundoparcial/base_de_datos/UsuarioDao.java
+++ app/src/main/java/com/example/carlos/segundoparcial/base_de_datos/UsuarioDao.java
@@ -1,22 +1,23 @@
 package com.example.carlos.segundoparcial.base_de_datos;
 
-import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
-import com.example.carlos.segundoparcial.Noticias;
-
-import java.util.List;
+import com.example.carlos.segundoparcial.Usuario;
 
 @Dao
-public interface noticiasDao {
-
+public interface UsuarioDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insert(Noticias... noticias);
+    void insertar(Usuario usuario);
 
-    @Query("SELECT * FROM tabla_noticias")
-    LiveData<List<Noticias>> getNoticias();
+    @Update
+    void actualizar(Usuario usuario);
+
+    @Delete
+    void eliminar(Usuario usuario);
 
 }
