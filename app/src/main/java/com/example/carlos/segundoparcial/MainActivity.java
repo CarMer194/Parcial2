201e5160540ec57ab02fa325a8cdccd561d894aa
CarMer194 <00060313@uca.edu.sv>
2018-06-07 20:27:46 -0600

    agregado el navigationview

--- app/src/main/java/com/example/carlos/segundoparcial/MainActivity.java
+++ app/src/main/java/com/example/carlos/segundoparcial/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.carlos.segundoparcial;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        final DrawerLayout drawerLayout = findViewById(R.id.main_drawer);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar, R.string.openDrawer,R.string.closeDrawer);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.main_navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id= item.getItemId();
+
+                if (id==R.id.menu_noticias_generales){
+                    //iniciar noticias generales
+                }
+                if (id==R.id.menu_favoritos){
+                    //iniciar favoritos
+                }
+                if (id==R.id.menu_ajustes){
+                    //iniciar ajustes
+                }
+                if (id==R.id.menu_juegos){
+                    //iniciar juegos
+                }
+                DrawerLayout drawerLayout1 = findViewById(R.id.main_drawer);
+                drawerLayout1.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
     }
 }
