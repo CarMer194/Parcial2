5ea9143d8b1a0b0eb4022dd7b5c3b925c2b10ef5
CarMer194 <00060313@uca.edu.sv>
2018-06-08 01:23:19 -0600

    agregado noticias dao

--- app/src/main/java/com/example/carlos/segundoparcial/Noticias.java
+++ app/src/main/java/com/example/carlos/segundoparcial/Noticias.java
@@ -1,4 +1,35 @@
 package com.example.carlos.segundoparcial;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "tabla_noticias")
 public class Noticias {
+
+    @NonNull
+    @PrimaryKey
+    @ColumnInfo(name = "id_noticia")
+    private int id;
+
+     @ColumnInfo(name = "titulo_noticia")
+    private String titulo;
+
+     @ColumnInfo(name = "imagen_noticia")
+    private String imagen;
+
+     @ColumnInfo(name = "fecha_noticia")
+    private String fecha;
+
+     @ColumnInfo(name = "resumen_noticia")
+    private String resumen;
+
+     @ColumnInfo(name = "contenido_noticia")
+    private String contenido;
+
+     @ColumnInfo(name = "juego_noticia")
+    private String juego;
+
 }
