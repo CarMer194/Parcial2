702ca5203237b07f56be5e721eae602c30038115
CarMer194 <00060313@uca.edu.sv>
2018-06-08 02:03:10 -0600

    agregado apicomu

---
+++ app/src/main/java/com/example/carlos/segundoparcial/retrofits/ApiComu.java
@@ -0,0 +1,35 @@
+package com.example.carlos.segundoparcial.retrofits;
+
+import com.example.carlos.segundoparcial.Noticias;
+import com.example.carlos.segundoparcial.Token;
+import com.example.carlos.segundoparcial.Usuario;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Url;
+
+public interface ApiComu {
+
+    String ENDPOINT = "http://gamenewsuca.herokuapp.com";
+
+    @FormUrlEncoded
+    @POST("/login")
+    Call<Token> iniciarSesion(@Field("user") String string,@Field("password") String contra);
+
+
+    @POST
+    Single<ResponseBody> agregarUsuario(@Url String url, @Body Usuario usuario);
+
+    @GET("/news")
+    Call<List<Noticias>> getNoticias(@Header("Authorization") String token);
+
+}
